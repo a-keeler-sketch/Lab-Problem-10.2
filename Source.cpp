85bff47165f7f0e82407d2c798620d73c6d6b365
Anna Keeler <a-keeler@onu.edu>
2025-11-06 12:59:04 -0500

    Finished P10-2

--- Source.cpp
+++ Source.cpp
@@ -1,24 +1,52 @@
 /*
-* File: P10_2
+* File: P10-2
 * Author: Anna Keeler
-* Date: 11/6/2025
+* Date: 10/27/2025
 *
-* Description: Lab 10.2
+* Description: Lab Problem 10 part 2
 */
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include <string>
 using namespace std;
-//global variables
+//prototype
 
-//prototypes
+int countWords(string str);
 
+//global variable
 
+string input; // get correct word count for however many spaces
 
 int main(void) {
-    
+
+    //DELETE EVERYTHING BEFORE FIRST COMMIT
+    //DELETE THIS FOR THE GITHUB COMMIT, YOU WILL NEED IT LATER THO TO GET USER DATA
+    while (true) {
+        cout << "Enter a string or Q to quit: ";
+        getline(cin, input);
+        if (input == "Q") break;
+
+
+        cout << "Word Count: " << countWords(input) << endl;
+    }
+
     return 0;
 
 }
 
+int countWords(string str) {
+    str.length();
+    int count = 0;
+    for (int i = 0; i < str.length(); i++) {
+
+        if (input[i] == ' ' && input[i + 1] != ' ') {
+            count++;
+        }
+
+    }
+    count += 1;
+
+    return count;
+
+}
