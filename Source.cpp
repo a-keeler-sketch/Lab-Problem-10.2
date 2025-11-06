b9f68376873fb857c1b6f751c3097555e333fd37
Anna Keeler <a-keeler@onu.edu>
2025-11-06 12:41:32 -0500

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,23 @@
+/*
+* File: P10_2
+* Author: Anna Keeler
+* Date: 11/6/2025
+*
+* Description: Lab 10.2
+*/
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+//global variables
+
+//prototypes
+
+
+int main(void) {
+    
+    return 0;
+
+}
+
